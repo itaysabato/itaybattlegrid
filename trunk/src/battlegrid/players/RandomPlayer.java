cf51f0a6a32e32248b9c48aa3625874ceb103221
ItaySabato <ItaySabato@d2cd6cd0-64c8-27b6-3528-3b6b17b9ad82>
2011-01-26 19:32:59 +0000

    update

---
+++ trunk/src/battlegrid/players/RandomPlayer.java
@@ -0,0 +1,30 @@
+package battlegrid.players;
+
+import battlegrid.abstracts.ActionHolder;
+import battlegrid.abstracts.GameEntityInfo;
+import battlegrid.abstracts.Player;
+import battlegrid.game.execution.Game;
+
+import java.util.Random;
+
+/**
+ * Names: Itay Sabato, Rotem Barzilay <br/>
+ * Logins: itays04, rotmus <br/>
+ * IDs: 036910008, 300618592 <br/>
+ * Date: 24/01/2011 <br/>
+ * Time: 17:03:10 <br/>
+ */
+public class RandomPlayer implements Player  {
+    private Random generator;
+
+    public  RandomPlayer(){
+        generator = new Random();
+    }
+
+    public void init(GameEntityInfo[][] gameState, GameEntityInfo myState) {}
+
+    public void doAction(GameEntityInfo[][] gameState, GameEntityInfo myState, ActionHolder toDo) {
+        int x  = generator.nextInt(Game.Action.values().length - 1);
+        toDo.setAction(Game.Action.values()[x+1]);
+    }
+}
