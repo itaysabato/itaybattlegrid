4be3f30c3af9fb19c34e12fbdd649bf2db260da9
ItaySabato <ItaySabato@d2cd6cd0-64c8-27b6-3528-3b6b17b9ad82>
2011-01-30 22:00:31 +0000

    update

---
+++ trunk/src/battlegrid/players/CowardlyPlayer.java
@@ -0,0 +1,22 @@
+package battlegrid.players;
+
+import battlegrid.abstracts.ActionHolder;
+import battlegrid.abstracts.GameEntityInfo;
+import battlegrid.abstracts.Player;
+
+/**
+ * Names: Itay Sabato, Rotem Barzilay <br/>
+ * Logins: itays04, rotmus <br/>
+ * IDs: 036910008, 300618592 <br/>
+ * Date: 30/01/2011 <br/>
+ * Time: 23:52:00 <br/>
+ */
+public class CowardlyPlayer implements Player {
+    public void init(GameEntityInfo[][] gameState, GameEntityInfo myState) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public void doAction(GameEntityInfo[][] gameState, GameEntityInfo myState, ActionHolder toDo) {
+        
+    }
+}
