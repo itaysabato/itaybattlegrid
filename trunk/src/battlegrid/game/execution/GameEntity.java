b7947b5bb02c2119173c72c4d34f83691acf053e
ItaySabato <ItaySabato@d2cd6cd0-64c8-27b6-3528-3b6b17b9ad82>
2011-02-08 17:38:07 +0000

    update

--- trunk/src/battlegrid/game/execution/GameEntity.java
+++ trunk/src/battlegrid/game/execution/GameEntity.java
@@ -78,8 +78,4 @@ class GameEntity implements GameEntityInfo {
     public boolean hasDirection() {
         return false;
     }
-
-    public static void main(String[] args) {
-        System.out.println(8 % 7);
-    }
 }
---
+++ trunk/src/battlegrid/players/AStarPlayer.java
@@ -0,0 +1,56 @@
+package battlegrid.players;
+
+import battlegrid.abstracts.ActionHolder;
+import battlegrid.abstracts.GameEntityInfo;
+import battlegrid.abstracts.Player;
+import battlegrid.game.execution.Game;
+
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+/**
+ * Names: Itay Sabato, Rotem Barzilay <br/>
+ * Logins: itays04, rotmus <br/>
+ * IDs: 036910008, 300618592 <br/>
+ * Date: 08/02/2011 <br/>
+ * Time: 14:48:37 <br/>
+ */
+public class AStarPlayer implements Player {
+
+    private int maxDepth;
+    private StateFactory stateFactory;
+    private Queue<State> active = new PriorityQueue<State>();
+
+
+    public void init(GameEntityInfo[][] gameState, GameEntityInfo myState) {
+        maxDepth = 300;
+        stateFactory = new AStarStateFactory();
+    }
+
+    public void doAction(GameEntityInfo[][] gameState, GameEntityInfo myState, ActionHolder toDo) {
+        active.clear();
+        active.addAll(stateFactory.getInitialStates(gameState,myState));
+
+        for(int i = 0; i < maxDepth; i++){
+            State best = active.remove();
+            if(best.getHeuristicValue() == 0){
+                toDo.setAction(best.getRootAction());
+                return;
+            }
+            active.addAll(best.spawn());
+        }
+        toDo.setAction(active.remove().getRootAction());
+    }
+
+    interface State extends Comparable<State> {
+        Game.Action getRootAction();
+        List<State> spawn();
+        int getHeuristicValue();
+        int getPathValue();
+    }
+
+    interface StateFactory {
+        List<State> getInitialStates(GameEntityInfo[][] gameState, GameEntityInfo myState);
+    }
+}
