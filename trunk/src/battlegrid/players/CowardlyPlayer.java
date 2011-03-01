122a0c839c3b78ed654ce77afa5bfe528b6a8940
ItaySabato <ItaySabato@d2cd6cd0-64c8-27b6-3528-3b6b17b9ad82>
2011-03-01 22:55:32 +0000

    update

--- trunk/src/battlegrid/players/CowardlyPlayer.java
+++ trunk/src/battlegrid/players/CowardlyPlayer.java
@@ -2,7 +2,9 @@ package battlegrid.players;
 
 import battlegrid.abstracts.ActionHolder;
 import battlegrid.abstracts.GameEntityInfo;
+import battlegrid.abstracts.GameEntityType;
 import battlegrid.abstracts.Player;
+import battlegrid.game.execution.Game;
 
 import java.util.Map;
 
@@ -14,6 +16,7 @@ import java.util.Map;
  * Time: 23:52:00 <br/>
  */
 public class CowardlyPlayer implements Player {
+
     public void setAttributes(Map<String, String> playerAttributes) {
         //To change body of implemented methods use File | Settings | File Templates.
     }
@@ -23,7 +26,26 @@ public class CowardlyPlayer implements Player {
     }
 
     public void doAction(GameEntityInfo[][] gameState, GameEntityInfo myState, ActionHolder toDo) {
-        
+          if(calcLocked(gameState, myState)){
+              toDo.setAction(Game.Action.SHOOT);
+          }
+//        else if()
+    }
+
+
+        private boolean calcLocked(GameEntityInfo[][] gameState, GameEntityInfo shooter) {
+        int x = shooter.getX();
+        int y = shooter.getY();
+        do{
+            x += shooter.getDirection().dx;
+            y += shooter.getDirection().dy;
+        } while(gameState[y][x].getType().equals(GameEntityType.BLANK));
+        if(gameState[y][x].getType().equals(GameEntityType.PLAYER)){
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     public void gameOver(boolean youWin) {
