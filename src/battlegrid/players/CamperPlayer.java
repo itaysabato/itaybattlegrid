170c9a15ff63bbf82887dd9bda797536eb8725d6
ItaySabato <ItaySabato@d2cd6cd0-64c8-27b6-3528-3b6b17b9ad82>
2011-03-01 20:31:02 +0000

    update

--- src/battlegrid/players/CamperPlayer.java
+++ src/battlegrid/players/CamperPlayer.java
@@ -1,7 +1,8 @@
 package battlegrid.players;
 
-import battlegrid.abstracts.*;
-import battlegrid.game.execution.Game;
+import battlegrid.abstracts.ActionHolder;
+import battlegrid.abstracts.GameEntityInfo;
+import battlegrid.abstracts.Player;
 
 import java.util.Map;
 
@@ -9,42 +10,20 @@ import java.util.Map;
  * Names: Itay Sabato, Rotem Barzilay <br/>
  * Logins: itays04, rotmus <br/>
  * IDs: 036910008, 300618592 <br/>
- * Date: 28/01/2011 <br/>
- * Time: 18:13:29 <br/>
+ * Date: 01/03/2011 <br/>
+ * Time: 22:29:52 <br/>
  */
 public class CamperPlayer implements Player {
-
-    private int turns;
-
     public void setAttributes(Map<String, String> playerAttributes) {
         //To change body of implemented methods use File | Settings | File Templates.
     }
 
     public void init(GameEntityInfo[][] gameState, GameEntityInfo myState) {
-        turns = 0;
+        //To change body of implemented methods use File | Settings | File Templates.
     }
 
     public void doAction(GameEntityInfo[][] gameState, GameEntityInfo myState, ActionHolder toDo) {
-        int x = myState.getX();
-        int y = myState.getY();
-        do{
-            x += myState.getDirection().dx;
-            y += myState.getDirection().dy;
-        } while(gameState[y][x].getType().equals(GameEntityType.BLANK));
-        if(gameState[y][x].getLife() > 0) {
-            toDo.setAction(Game.Action.SHOOT);
-        }
-        else {
-            boolean isFrontClear = gameState[myState.getDirection().dy + myState.getY()][myState.getDirection().dx + myState.getX()].getType().equals(GameEntityType.BLANK);
-            if(turns < Direction.values().length || !isFrontClear){
-                toDo.setAction(Game.Action.TURN_LEFT);
-                turns++;
-            }
-            else {
-                toDo.setAction(Game.Action.MOVE_FWD);
-                turns = 0;
-            }
-        }
+        //To change body of implemented methods use File | Settings | File Templates.
     }
 
     public void gameOver(boolean youWin) {
